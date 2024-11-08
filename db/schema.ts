ef01bd7f1c40e28e0b30f9dd99e1df019fddf9af
Brian Baso <brianbaso@gmail.com>
2024-11-08 12:55:01 +0700

    add journal, analysis schema

--- db/schema.ts
+++ db/schema.ts
@@ -1,6 +1,6 @@
 import { Message } from "ai";
 import { InferSelectModel } from "drizzle-orm";
-import { pgTable, varchar, timestamp, json, uuid } from "drizzle-orm/pg-core";
+import { pgTable, varchar, timestamp, json, uuid, text } from "drizzle-orm/pg-core";
 
 export const user = pgTable("User", {
   id: uuid("id").primaryKey().notNull().defaultRandom(),
@@ -22,3 +22,20 @@ export const chat = pgTable("Chat", {
 export type Chat = Omit<InferSelectModel<typeof chat>, "messages"> & {
   messages: Array<Message>;
 };
+
+export const journal = pgTable("Journal", {
+  id: uuid("id").primaryKey().notNull().defaultRandom(),
+  createdAt: timestamp("createdAt").notNull(),
+  userId: uuid("userId").notNull().references(() => user.id),
+  entryText: text("entryText").notNull()
+});
+
+export type Journal = InferSelectModel<typeof journal>;
+
+export const analysis = pgTable("Analysis", {
+  id: uuid("id").primaryKey().notNull().defaultRandom(),
+  journalId: uuid("journalId").notNull().references(() => journal.id),
+  analysisText: text("analysisText").notNull(),
+});
+
+export type Analysis = InferSelectModel<typeof analysis>;
