0196d902c0e82a89fa49fc0870efb77931fc1223
Brian Baso <brianbaso@gmail.com>
2024-11-19 15:21:52 +0700

    lint fix

--- components/custom/markdown.tsx
+++ components/custom/markdown.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import React, { memo } from "react";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
+
 import DefinitionBlock from "./definition-block";
 
 const NonMemoizedMarkdown = ({ children }: { children: string }) => {
